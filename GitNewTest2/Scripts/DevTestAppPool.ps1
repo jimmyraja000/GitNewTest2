e68cdad317ff2ba73ea6919a47e017f377148d42
jimmyraja000 <92245249+jimmyraja000@users.noreply.github.com>
2023-04-28 13:11:54 +0530

    Create DevTestAppPool.ps1

---
+++ GitNewTest2/Scripts/DevTestAppPool.ps1
@@ -0,0 +1,17 @@
+import-module webadministration
+
+$DEV_APP_POOL_NAME="DevAPP_POOL_NAME"
+
+if(Test-Path IIS:\AppPools\$AppPoolName)
+{
+"AppPool is already there"
+return $true;
+}
+else
+{
+"AppPool is not present"
+"Creating new AppPool"
+Import-Module WebAdministration
+New-WebAppPool "$DEV_APP_POOL_NAME" -Force
+return $false;
+}
