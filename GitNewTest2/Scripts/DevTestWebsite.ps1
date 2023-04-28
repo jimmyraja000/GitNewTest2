7e81125d587b3ec336fa44c71d94921a203734ff
jimmyraja000 <92245249+jimmyraja000@users.noreply.github.com>
2023-04-28 13:14:01 +0530

    Create DevTestWebsite.ps1

---
+++ GitNewTest2/Scripts/DevTestWebsite.ps1
@@ -0,0 +1,17 @@
+import-module webadministration
+$DEV_APP_POOL_NAME= "DevAPP_POOL_NAME"
+$DEV_WEBSITE_NAME="DevGit_New_Test2"
+
+if(Test-Path IIS:\Sites\$DEV_WEBSITE_NAME)
+{
+"WebSite is already there"
+return $true;
+}
+else
+{
+"Website is not present"
+"Creating new WebSite"
+Import-Module WebAdministration
+New-Website -Name "$DEV_WEBSITE_NAME" -PhysicalPath "C:\inetpub\wwwroot\$DEV_WEBSITE_NAME" -ApplicationPool "$DEV_APP_POOL_NAME" -Force
+return $false;
+}
