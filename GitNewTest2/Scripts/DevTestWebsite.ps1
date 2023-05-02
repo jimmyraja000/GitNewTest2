2661b61e0f160fe96bf83336fe0e9aab9c7220f8
jimmyraja000 <92245249+jimmyraja000@users.noreply.github.com>
2023-05-02 10:23:31 +0530

    Update DevTestWebsite.ps1

--- GitNewTest2/Scripts/DevTestWebsite.ps1
+++ GitNewTest2/Scripts/DevTestWebsite.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy RemoteSigned
 import-module webadministration
 $DEV_APP_POOL_NAME= "DevAPP_POOL_NAME"
 $DEV_WEBSITE_NAME="DevGit_New_Test2"
@@ -11,7 +12,6 @@ else
 {
 "Website is not present"
 "Creating new WebSite"
-Import-Module WebAdministration
 New-Website -Name "$DEV_WEBSITE_NAME" -PhysicalPath "C:\inetpub\wwwroot\$DEV_WEBSITE_NAME" -ApplicationPool "$DEV_APP_POOL_NAME" -Force
 return $false;
 }
